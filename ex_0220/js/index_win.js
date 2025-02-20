419f6580eafabc53d645b4b53871e39ad08a6df8
Yoojin-J <syoojin03@gmail.com>
2025-02-20 16:22:14 +0900

    window, windowscreen

---
+++ ex_0220/js/index_win.js
@@ -0,0 +1,33 @@
+// ex1
+document.getElementById("demo1").innerHTML = "Browser inner window width: " + window.innerWidth + "px<br>" +
+"Browser inner window height: " +  window.innerHeight + "px";
+
+
+
+// ex2
+document.getElementById("demo2").innerHTML = "Screen width is " + screen.width;
+
+
+
+// ex3
+document.getElementById("demo3").innerHTML = "Screen height is " + screen.height;
+
+
+
+// ex4
+document.getElementById("demo4").innerHTML = "Available screen width is " + screen.availWidth;
+
+
+
+// ex5
+document.getElementById("demo5").innerHTML = "Available screen height is " + screen.availHeight;
+
+
+
+// ex6
+document.getElementById("demo6").innerHTML = "Screen color depth is " + screen.colorDepth;
+
+
+
+// ex7
+document.getElementById("demo7").innerHTML = "Screen pixel depth is " + screen.pixelDepth;
